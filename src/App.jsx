0f51d3d2ceb3db4d5abfa68459e5294a2004f824
Ptarsariya176 <parthtarsariya.it22@scet.ac.in>
2025-01-06 15:09:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
   const [count, setcount] = useState(0);
   return (
     <div className="App">
-      <button onClick={() => setcount(count + 1)}>value of count is {count}</button>
+      <button onClick={() => setcount(count + 1)}>value of count is  increment{count}</button>
+      <br />
+      <button onClick={()=> setcount(count-1)}> value of count is decryment  {count}</button>
     </div>
   );
 }
